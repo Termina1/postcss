7feeb76d32cd28aadcc0463c529ecbabcee57353
Termina1 <terminal2010@gmail.com>
2014-10-29 14:24:00 +0300

    add async support

--- test/postcss.js
+++ test/postcss.js
@@ -1,6 +1,8 @@
 var postcss = require('../lib/postcss');
 var Result  = require('../lib/result');
 var Root    = require('../lib/root');
+var Promise = require('bluebird');
+var should  = require('should');
 
 describe('postcss.root()', () => {
 
@@ -115,7 +117,7 @@ describe('postcss()', () => {
         it('returns Result object', () => {
             var result = postcss().process('a{}');
             result.should.be.an.instanceOf(Result);
-            result.css.should.eql(       'a{}');
+            result.css.should.eql('a{}');
             result.toString().should.eql('a{}');
         });
 
@@ -136,6 +138,103 @@ describe('postcss()', () => {
         it('send options to processors', () => {
             var a = (css, opts) => opts.should.eql({ from: 'a.css' });
             postcss(a).process('a {}', { from: 'a.css' });
+        });
+
+    });
+
+    describe('async()', () => {
+        before( () => {
+            this.processor = postcss( (css, opts, done) => {
+                css.eachRule( (rule) => {
+                    if ( !rule.selector.match(/::(before|after)/) ) return;
+                    if ( !rule.some( i => i.prop == 'content' ) ) {
+                        rule.prepend({ prop: 'content', value: '""' });
+                    }
+                });
+                setTimeout(done, 00);
+            });
+        });
+
+        should.Assertion.add('eventuallyEql', function(str, done) {
+            this.obj.then(function(result) {
+                result.css.should.eql(str);
+                done();
+            }).catch(function(error) {
+                done(error);
+            });
+        });
+
+        function eventually(result) {
+            return Promise.resolve(result.css);
+        }
+
+        it('processes CSS', (cb) => {
+            var result = this.processor.async('a::before{top:0}');
+            result.should.eventuallyEql('a::before{content:"";top:0}', cb);
+        });
+
+        it('processes parsed AST', (cb) => {
+            var root   = postcss.parse('a::before{top:0}');
+            var result = this.processor.async(root);
+            result.should.eventuallyEql('a::before{content:"";top:0}', cb);
+        });
+
+        it('processes previous result', (cb) => {
+            var empty  = postcss( (css) => css );
+            var result = empty.process('a::before{top:0}');
+            result = this.processor.async(result);
+            result.should.eventuallyEql('a::before{content:"";top:0}', cb);
+        });
+
+        it('throws with file name', (cb) => {
+            postcss().async('a {', { from: 'A' }).catch(function(error) {
+                try {
+                    error.file.should.eql('A');
+                    error.message.should.eql('A:1:1: Unclosed block');
+                    cb();
+                } catch(e) {
+                    cb(e);
+                }
+            });
+
+        });
+
+        it('allows to replace Root', (cb) => {
+            var processor = postcss( () => new Root() );
+            processor.async('a {}').should.eventuallyEql('', cb);
+        });
+
+        it('returns Result object', (cb) => {
+            var result = postcss().async('a{}').then((result) => {
+                result.should.be.an.instanceOf(Result);
+                result.css.should.eql('a{}');
+                result.toString().should.eql('a{}');
+                cb();
+            }).catch(error => cb(error));
+        });
+
+        it('calls all processors', (cb) => {
+            var calls = '';
+            var a = () => calls += 'a';
+            var b = () => calls += 'b';
+
+            postcss(a, b).async('')
+                .then( () => calls.should.eql('ab'))
+                .then(cb.bind(null, null))
+                .catch(error => cb(error));
+        });
+
+        it('parses, convert and stringify CSS', (cb) => {
+            var a = (css) => css.should.be.an.instanceof(Root);
+            postcss(a).async('a {}').then(result => {
+                result.css.should.have.type('string');
+            }).then(cb.bind(null, null)).catch(cb);
+        });
+
+        it('send options to processors', (cb) => {
+            var a = (css, opts) => opts.should.eql({ from: 'a.css' });
+            postcss(a).async('a {}', { from: 'a.css' })
+                .then(cb.bind(null, null)).catch(cb);
         });
 
     });
